4b674aef9337f3c6371cff690e91e53080fb9ed6
rafa gonzalez <rafael.gonzalez.1737@gmail.com>
2021-08-26 13:34:03 +0200

    new inputs

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BEM } from "../../functions";
+import { IInputProps } from "../../props/IInputProps";
+
+export const Input = ({ type = "text", ...props }: IInputProps) => {
+  const cs = new BEM("Input", {});
+  cs.Append(props.className);
+  return (
+    <input
+      className={ cs.toString() }
+      style={ props.style }
+      type={ type }
+      onChange={ props.onChange }
+      value={ props.value }
+      placeholder={ props.placeholder }
+      disabled={ props.disabled }
+      readOnly={ props.readOnly }
+    />
+  );
+};
