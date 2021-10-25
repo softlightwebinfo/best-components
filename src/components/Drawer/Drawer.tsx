ca8977e99a2509e66fa5fb47ef6f16e810592c36
rafa gonzalez <rafael.gonzalez.1737@gmail.com>
2021-10-25 13:35:12 +0200

    fix

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -11,21 +11,24 @@ export const Drawer = ({ isCloseable = true, ...props }: IDrawerProps) => {
   const ref = useRef(null);
   const [open, setOpen] = useState(false);
   const [close, setClose] = useState(true);
-
+  console.log(props, isCloseable);
   useEffect(() => {
     if (!isCloseable) return;
+    console.log(1, props, isCloseable);
     if (props.open) {
       setClose(false);
     }
-  }, [props.open, isCloseable]);
+  }, [props.open]);
 
   useTimeout(() => {
     if (!isCloseable) return;
+    console.log(2, props, isCloseable);
     setOpen(props.open);
   }, 1, props.open);
 
   useEffect(() => {
     if (!isCloseable) return;
+    console.log(3, props, isCloseable);
     if (!open) {
       setTimeout(() => {
         setClose(true);
@@ -44,6 +47,7 @@ export const Drawer = ({ isCloseable = true, ...props }: IDrawerProps) => {
     open: open,
     onClose: () => {
       if (!isCloseable) return;
+      console.log(4, props, isCloseable);
       setOpen(false);
       props.onClose?.();
     },
@@ -51,6 +55,7 @@ export const Drawer = ({ isCloseable = true, ...props }: IDrawerProps) => {
 
   useOutside(ref, props.open, () => {
     if (!isCloseable) return;
+    console.log(5, props, isCloseable);
     value.onClose();
   });
 
